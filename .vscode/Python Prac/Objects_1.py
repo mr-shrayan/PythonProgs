7611c007f547f7ed11d82f159d1f4ec7f50ab0c9
Shrayan Bandyopadhyay <theemptyunexplored@gmail.com>
2020-12-14 21:34:47 +0530

    L3 Python

--- .vscode/Python Prac/Objects_1.py
+++ .vscode/Python Prac/Objects_1.py
@@ -5,10 +5,12 @@ class PlayerCharacter:
         self.name = name
 
     def run(self):
-        print('run')
+        print('run')#it dosent return anything 
+        return('done')
 
 player1 = PlayerCharacter('shrayan')
 player2 = PlayerCharacter('Tom')
 
 print(player1.name)
 print(player2.name)
+print(player1.run())
