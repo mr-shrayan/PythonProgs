5d9c30552df0ae4b061f19c1c466bed171c159f8
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-18 22:10:31 +0530

    Decorators @Classmethod Example

--- .vscode/Decorators.py
+++ .vscode/Decorators.py
@@ -12,14 +12,15 @@ class PlayerCharacter:
 
     @classmethod
     def adding_things(cls, num1, num2):
-     return cls('Tommy' , num1 + num2)
+     return num1+num2
 # the only difference between the two is that @classmethod has access to the class body and does care about its parameters 
 # whereas @staticmethod is self dependent and has no access to cls or the class parameters.
-     @staticmethod 
-    def adding_things(num1, num2):
-     return num1 + num2
+   # @staticmethod
+   # def adding_things2(num1,num2):
+  #      return num1+num2
 
+# player1= PlayereCharacter('Tom', 20)
 
-player3 = PlayerCharacter.adding_things(2,3)
+print(PlayerCharacter.adding_things(2,3))
 
-print(player3.age)
+#print(player3.age)
