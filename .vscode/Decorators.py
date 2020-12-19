5dde39dc7a9b12912a1952c249b279bc4b6474c8
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-19 13:36:27 +0530

    Test

--- .vscode/Decorators.py
+++ .vscode/Decorators.py
@@ -14,7 +14,7 @@ class PlayerCharacter:
     def adding_things(cls, num1, num2):
      return cls('Tommy' , num1 + num2)
 # the only difference between the two is that @classmethod has access to the class body and does care about its parameters 
-# whereas @staticmethod is self dependent and has no access to cls or the class parameters.
+# whereas @staticmethod is self dependent and has no access to cls or the class parameters. thus cls is not required
     @staticmethod
     def adding_things2(num1,num2):
         return num1+num2
