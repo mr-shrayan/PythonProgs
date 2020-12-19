943da11bae425f4a083a7aa0528fb37ea57d845a
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-20 00:20:36 +0530

    PrivateVsPublic 2

--- PrivateVsPublic.py
+++ PrivateVsPublic.py
@@ -1,19 +1,22 @@
 #oop
+# The underscore before calling a variable has a convention in the python community that it is a private variable so no kind of alterations 
+# Should be made to those variables. 
 class PlayerCharacter:
     def __init__(self, name, age ):
-        self.name = name 
-        self.age = age
+        self._name = name 
+        self._age = age
 
     def run(self):
         print('run')
 
     def speak(self):
-        print(f'my name is{self.name} and my age is{self.age}')
+        print(f'my name is {self._name} and my age is {self._age}')
 
 player1=PlayerCharacter('shrayan',20)
 
-player1.name = ' !!!'
-player1.speak = ' BOOOOO'
-
-print (player1.speak) # The speak method is called directly without having to use speak() because player1.speak = ' BOOO00' is not
-#Inside a class. We know that is Object is inside of a class it becomes method.  
+# player1.name = ' !!!'
+# player1.speak = ' BOOOOO' 
+# Using the above 2 lines is not a problem and will not create an error but it will not follow the python community convention. 
+# It is important to follow conventions of a community while coding those languages in order to make other programmers familiar and comfortable
+# with the code that you have written.
+print (player1.speak()) 
