7e1843645afa524098d007e72ad65ff1db782132
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-20 20:20:09 +0530

    Inheritance 1.2

--- Inheritance.py
+++ Inheritance.py
@@ -3,9 +3,19 @@ class User():
         print('Logged in')
 
 class Wizard(User):
-    pass
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+    def attack(self):
+        print(f'{self.name} Attacking with a power of {self.power}')
 class Archer(User):
-    pass
+    def __init__(self, name, arrows):
+        self.name = name
+        self.arrows = arrows
+    def attack(self):
+        print(f'{self.name} Attacking with {self.arrows} arrows')
 
-wizard1 = Wizard()
-print(wizard1.sign_in())
+wizard1 = Wizard('Marlyn', 200)
+archer1 = Archer('Ford', 500)
+wizard1.attack()
+archer1.attack()
