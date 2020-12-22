153ed728675d4854f5e98bdf1fdbcc90805de329
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-22 14:13:25 +0530

    Polymorphism 2

--- Polymorphism.py
+++ Polymorphism.py
@@ -2,11 +2,15 @@ class User():
     def sign_in(self):
         print('Logged in')
 
+    def attack(self):
+        print('Do Nothing')
+
 class Wizard(User):
     def __init__(self, name, power):
         self.name = name
         self.power = power
     def attack(self):
+        User.attack(self)
         print(f'{self.name} Attacking with a power of {self.power}')
 class Archer(User):
     def __init__(self, name, arrows):
@@ -17,7 +21,7 @@ class Archer(User):
 
 wizard1 = Wizard('Marlyn', 200)
 archer1 = Archer('Ford', 500)
-# wizard1.attack()
+wizard1.attack()
 # archer1.attack()
 
 #Polymorphism
