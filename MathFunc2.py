90a3d21ffd23ec138150b0ad76cc7de4043d4356
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-25 12:39:09 +0530

    Math Functions

---
+++ MathFunc2.py
@@ -0,0 +1,44 @@
+def add(num1, num2):
+    return num1 + num2
+
+def substract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+operations = {
+            '+':'add',
+            '-':'Substract',
+            '/':'divide',
+            '':'multiply'
+            }
+
+def math_function():
+    while True:
+        num1 = int(input("Please Enter the first Number: "))
+        for key in operations:
+            print(key)
+        user_input = input("pick an operation from the line above: ")
+        num2 = int(input("Please Enter the Second Number: "))
+
+        if user_input == '+':
+            result = add(num1, num2)
+        elif user_input == '-':
+            result = substract(num1, num2)
+        elif user_input == '/':
+            result = divide(num1, num2)
+        elif user_input == '':
+            result = multiply(num1, num2)
+        print(f"{num1} {user_input} {num2} = {result} ")
+
+        response = input("would you like to do another manupulation")
+        if response == 'y':
+            math_function()
+            break
+        else:
+            break
+math_function()
