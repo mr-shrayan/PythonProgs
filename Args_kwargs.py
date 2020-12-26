318053561b46cd89f2d7734897e1f4fdc10e7b3b
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-26 18:13:58 +0530

    All progs

---
+++ Args_kwargs.py
@@ -0,0 +1,11 @@
+def Func1(arg1, arg2, arg3): 
+    print("arg1:", arg1) 
+    print("arg2:", arg2) 
+    print("arg3:", arg3) 
+      
+# Using *args or **kwargs to pass arguments to this function
+args = ("Sarah", "Jacob", "Alison") 
+Func1(*args) 
+  
+kwargs = {"arg1" : "Sarah", "arg2" : "Jacob", "arg3" : "Alison"} 
+Func1(**kwargs) 
