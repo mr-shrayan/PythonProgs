c86ff12a21fd507d05570a636f7e344819f05a1b
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-30 12:03:58 +0530

    Changing dunders

--- Dunders.py
+++ Dunders.py
@@ -2,6 +2,8 @@ class Toy():
     def __init__(self,color,age):
         self.color = color
         self.age = age
+    def __str__(self):
+        return f'{self.color}'
 
 action_figure = Toy('red', 0)
 
