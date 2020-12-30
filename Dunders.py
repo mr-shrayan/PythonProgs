296ce1fa1127e99713d19fd8e8bf477f65581e40
mr-shrayan <theemptyunexplored@gmail.com>
2020-12-30 18:53:43 +0530

    Dunder 3

--- Dunders.py
+++ Dunders.py
@@ -4,8 +4,11 @@ class Toy():
         self.age = age
     def __str__(self):
         return f'{self.color}'
+    def __len__(self):
+        return 5
 
 action_figure = Toy('red', 0)
 
 print (action_figure.__str__())
 print(str(action_figure))
+print(len(action_figure))
