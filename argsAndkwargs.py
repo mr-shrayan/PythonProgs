04ad9070baee0baa06a1b0d45d21090f67a4e64e
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-01 18:54:58 +0530

    kwargs aded

--- argsAndkwargs.py
+++ argsAndkwargs.py
@@ -1,5 +1,8 @@
-def super_func(*args):
-    print(*args)
-    print(args)
-    return sum(args)
-print(super_func(1,2,3,4,5))
+def super_func(*args,**kwargs):
+    # print(*args)
+    # print(kwargs)
+    total = 0
+    for items in kwargs.values():
+        total += items
+    return sum(args) + total
+print(super_func(1,2,3,4,5, num1  = 5, num2 = 10))
