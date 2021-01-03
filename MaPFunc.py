50b1b2f3cbbe0654a53bcc91e40f56acbbb63227
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-03 12:26:16 +0530

    Map

--- MaPFunc.py
+++ MaPFunc.py
@@ -1,10 +1,4 @@
-map
+def multiplyy_by2(item):
+    return item*2
 
-
-def multiply_by2(li):
-    new_list=[]
-    for items in li:
-        new_list.append(items*2)
-    return new_list
-
-print(multiply_by2([1,2,3]))
+print(list(map(multiplyy_by2,[1,2,3])))
