884b5e00b6090a63e443852f6d3de0dde78fa711
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-09 19:58:19 +0530

    reduce

--- Reduce.py
+++ Reduce.py
@@ -1,11 +1,10 @@
+from functools import reduce
 my_list= [1,2,3]
-your_list = [10,20,30]
-def multiply_by2(item):
-    return item*2
 
-def only_odd(item):
-    return item % 0 != 2
+def accumulator(acc, item):
+    print(acc, item)
+    return acc+item
 
-print(list(zip(my_list,your_list)))
+print(reduce(accumulator, my_list, 0))
 print(my_list)
-print(your_list)
+
