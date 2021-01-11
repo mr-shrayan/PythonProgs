67559b1a1ad41a4d1306c59cd55425abb09e7346
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-11 22:30:31 +0530

    List Comprehension

--- Comprehensions.py
+++ Comprehensions.py
@@ -1,1 +1,12 @@
 # List/ Set/ Dictionaries Comprehensions
+my_list = []
+
+for char in "hello":
+    my_list.append(char)
+
+print(my_list)
+
+#Or we can also write the program in a cleaner way such as 
+
+my_list2 = [char for char in "hello"]
+print(my_list2)
