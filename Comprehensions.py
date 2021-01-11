3235701772ae9104311ce8fe8a3ef6e44d5ebdf4
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-11 22:34:59 +0530

    Comprehension alterations

--- Comprehensions.py
+++ Comprehensions.py
@@ -9,4 +9,8 @@ print(my_list)
 #Or we can also write the program in a cleaner way such as 
 
 my_list2 = [char for char in "hello"]
+my_list3 = [num for num in range(0,200)]
+my_list4 = [num*2 for num in range(0,200)]
 print(my_list2)
+print(my_list3)
+print(my_list4)
