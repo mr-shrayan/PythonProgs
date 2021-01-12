47000dfd46b3b623802f173a0497b782898626af
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-12 17:51:42 +0530

    Exercise 4

--- Comprehensions.py
+++ Comprehensions.py
@@ -11,6 +11,8 @@ print(my_list)
 my_list2 = [char for char in "hello"]
 my_list3 = [num for num in range(0,200)]
 my_list4 = [num*2 for num in range(0,200)]
+my_list5 = [num*2 for num in range(0,200) if num%2 ==0]
 print(my_list2)
 print(my_list3)
 print(my_list4)
+print(my_list5)
