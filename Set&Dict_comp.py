d0f09a0264e2fb58275e62c1c6775824563713f3
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-13 16:11:30 +0530

    Sets Comprehension example(Commented out)

---
+++ Set&Dict_comp.py
@@ -0,0 +1,11 @@
+#Set and Dictionart comprehensions
+
+
+# my_list2 = {char for char in "hello"}#Sets does not allow any duplicates. Only unique items.
+# my_list3 = {num for num in range(0,200)}
+# # my_list4 = [num*2 for num in range(0,200)]
+# # my_list5 = [num*2 for num in range(0,200) if num%2 ==0]
+# print(my_list2)
+# print(my_list3)
+# # print(my_list4)
+# # print(my_list5)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,19 +1,3 @@
-#oop
-class PlayerCharacter:
-    
-    def __init__(self, name, age ):
-        self.name = name 
-        self.age = age
-
-    def run(self):
-        print('run')
-
-    def speak(self):
-        print(f'my name is{self.name} and my age is{self.age}')
-
-    player1=PlayerCharacter('shrayan',20)
-
-    player1.name = '!!!'
-    player1.age = 'BOOOOO'
-
-    print (player1.speak)
+my_list3 = [num for num in range(0,200)]
+# my_list4 = [num*2 for num in range(0,200)]
+# my_list5 = [num*2 for num in range(0,200) if num%2 ==0]
