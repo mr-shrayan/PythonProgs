08605a11935a1d1108c2a64afe449d0aa8b96da0
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-13 17:34:27 +0530

    Dictionary comprehensions 2

--- Set&Dict_comp.py
+++ Set&Dict_comp.py
@@ -3,14 +3,15 @@
 
 # my_list2 = {char for char in "hello"}#Sets does not allow any duplicates. Only unique items.
 # my_list3 = {num for num in range(0,200)}
-# # my_list4 = [num*2 for num in range(0,200)]
-# # my_list5 = [num*2 for num in range(0,200) if num%2 ==0]
+# my_list4 = [num*2 for num in range(0,200)]
+# my_list5 = [num*2 for num in range(0,200) if num%2 ==0]
 # print(my_list2)
 # print(my_list3)
-# # print(my_list4)
-# # print(my_list5)
+# print(my_list4)
+# print(my_list5)
 
 sampledict= {'a':1, 'b':2}
 my_dict = {key:value**2 for key,value in sampledict.items()}
+my_dict2 = {num:num*2 for num in [1,2,3]}
 
-print(my_dict)
+print(my_dict2)
