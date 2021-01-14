8c2bc2bf0db86b0a1345eb6bba9460eb1cadeb09
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-14 16:15:19 +0530

    Code runner

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-duplicates = []
-# for value in some_list:
-#     if some_list.count(value)>1:
-#         if value not in duplicates:
-#             duplicates.append(value)
-
-# print (duplicates)
+'a
