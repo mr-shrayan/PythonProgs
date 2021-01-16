ea3b555c7e9e8cdfe6ac59c48c3f1b9d4ec9c606
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-16 12:46:15 +0530

    Decorators2

--- Exercise5.py
+++ Exercise5.py
@@ -1,10 +1,10 @@
 some_list = ['a', 'b','c','b','d','m','n','n']
 
-# duplicates = []
-# for value in some_list:
-#     if some_list.count(value)>1:
-#         if value not in duplicates:
-#             duplicates.append(value)
+#  duplicates22 = []
+#  for value in some_list:
+#      if some_list.count(value)>1:
+#          if value not in duplicates:
+#              duplicates.append(value)
 
 # print (duplicates)
 # Converting the above line of code into a single line.
