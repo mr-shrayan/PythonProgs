580ff6dd803c72fc4a9120b0b56885be3cf44495
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-18 18:19:34 +0530

    Decorator Pattern

--- WritingDecorators.py
+++ WritingDecorators.py
@@ -1,8 +1,9 @@
-# Decorators just supercharges a function. The main function of a decorator is to give extra fuctionalities to a function.
+# Decorators just supercharges a function. The main function of a decorator is to give extra fuctionalities to a function.\
+#Decorator Pattern
 def my_decorator(func):
-    def wrap_func(x):
+    def wrap_func(*args,**kwargs):
         print('***********************')
-        func(x)
+        func(*args,**kwargs)
         print('***********************')
     return wrap_func
 #Now hello is accepting an argument.
