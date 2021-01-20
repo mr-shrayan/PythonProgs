7efbd85ef87eca801cb081bc5e33d0e2ba997af5
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-20 14:05:25 +0530

    Creating a Decorator and also using the time function

---
+++ PE_Decorators.py
@@ -0,0 +1,16 @@
+#Building a decorator.
+from time import time
+def performance(fn):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = fn(*args,**kwargs)
+        t2 = time()
+        print(f'It took {t2-t1} ms to run')
+        return result
+    return wrapper
+
+@performance
+def long_time():
+    for i in range(100000000):
+        i*5
+long_time()
