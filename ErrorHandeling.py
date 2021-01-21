255ec023e4946f9d354c401327695929bcce6e18
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-22 00:19:24 +0530

    Errors

--- ErrorHandeling.py
+++ ErrorHandeling.py
@@ -1,1 +1,5 @@
 # Video 155 cheacking out different types of errors and knowing how to solve the errors.
+print(1+'hello')
+pront(1+1)
+print(2/0)
+# These are a few examples of errors that a programmer can encounter while programming. 
