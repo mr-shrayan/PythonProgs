1e74b220b25da632ef2e549819634e003a5d9774
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-24 22:53:59 +0530

    Types of error

--- ErrorHandeling.py
+++ ErrorHandeling.py
@@ -3,3 +3,20 @@ print(1+'hello')
 pront(1+1)
 print(2/0)
 # These are a few examples of errors that a programmer can encounter while programming. 
+# Syntax Error
+def hooooooooo() #Fotgot to put colon 
+    pass
+
+def hooooooooo():
+    1+name #Name error
+
+def hooooooooooo():
+    li = [1,2,3]
+    li[5] #Index error
+
+def hooooooooooo():
+    di = {'a':1}
+    di['b']# Key error
+
+def hooooooooooo():
+    5/0 #Zero division error
