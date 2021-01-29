4101b8b6942a3fe0b919272450d10d6bbb2be058
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-29 22:44:00 +0530

    Error handeling

--- Error_Handeling2.py
+++ Error_Handeling2.py
@@ -6,7 +6,7 @@ while True:
          10/age # Entering age =0 creates a zero division errors
      except ValueError: # Except block runs only once and once it catches an error it stops
          print('Please enter a number')
-     except ZeroDivisionError: 
+     except ZeroDivisionError: #Mentioning the type of errors will lead to proper error handeling of a program. Find the proper type of errors to handle such kind
          print('Please enter age higher than 0')
      else:
          print('Thank You')
