f0c3d076798206f62312a8b69245200c1bf49a7b
mr-shrayan <theemptyunexplored@gmail.com>
2021-01-30 21:18:21 +0530

    Error handeling 157

---
+++ ErrorHandeling_157.py
@@ -0,0 +1,14 @@
+# def sum(num1,num2):
+#     return num1+num2
+
+# print(sum('1',2))
+
+# The above line of code will output an error since we are adding a string to an integer.
+# in order to handle any sort of error we need to use a try and catch block. 
+# we could use a try and except block on the print statement or instead the best practice would be to use the block inside the def function.
+def sum(num1,num2):
+    try:
+        return num1+num2
+    except:
+        print("Something is wrong! please input 2 integers.")
+print(sum('1',2))
