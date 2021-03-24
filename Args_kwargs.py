b764e2a5441521ad97298b94ab8f4fd490e30185
mr-shrayan <theemptyunexplored@gmail.com>
2021-03-24 20:29:23 +0530

    This is a test program

--- Args_kwargs.py
+++ Args_kwargs.py
@@ -1,7 +1,7 @@
 def Func1(arg1, arg2, arg3): 
     print("arg1:", arg1) 
     print("arg2:", arg2) 
-    print("arg3:", arg3) 
+    print("arg3:", arg3)  
       
 # Using *args or **kwargs to pass arguments to this function
 args = ("Sarah", "Jacob", "Alison") 
