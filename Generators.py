c92bc7841ada2019e6fd40004b743a47d8b0d72e
mr-shrayan <theemptyunexplored@gmail.com>
2021-03-24 20:46:47 +0530

    generators

---
+++ Generators.py
@@ -0,0 +1,14 @@
+# A generator creates memory spaces one at a time and does not occupy a whole lot of memory together. thats why the
+# function "range(100)" is also a generator
+
+# range(100)
+# list(range(100))
+
+def counter(num):
+    result = []
+    for i in range(num):
+        result.append(i*2)
+    return result
+
+my_list = counter(100)
+print(my_list)
