ac2845f1ea1f678ecc8c1a3ff5049102740fdeb0
mr-shrayan <theemptyunexplored@gmail.com>
2021-03-25 22:43:54 +0530

    Generators Update

--- Generators.py
+++ Generators.py
@@ -1,5 +1,5 @@
 # A generator creates memory spaces one at a time and does not occupy a whole lot of memory together. thats why the
-# function "range(100)" is also a generator
+# function "range(100)" is also a generator to print a list of the range we use the line list(range(100)) 
 
 # range(100)
 # list(range(100))
