3cda5a10e78eec51e6b64affb58e12952cc74c7d
mr-shrayan <theemptyunexplored@gmail.com>
2021-03-25 23:04:47 +0530

    revert

--- Filter.py
+++ Filter.py
@@ -2,7 +2,7 @@ my_list = [1,2,3]
 def multiply_by2(item):
     return item*2
 def only_odd(item):
-    return item % 2 !=1
+    return item % 2 !=0
 
 print(list(filter(only_odd, my_list)))
 # The command filters out the even number from the list since it returns the extression (item % 2 != 0) as false. 
