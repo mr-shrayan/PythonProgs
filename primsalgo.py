b9fa3ca980acbe4c21a53b134d6e628f11b06ee5
mr-shrayan <theemptyunexplored@gmail.com>
2021-05-27 16:01:46 +0530

    prims

---
+++ primsalgo.py
@@ -0,0 +1,45 @@
+# Prim's Algorithm in Python
+
+
+INF = 9999999
+# number of vertices in graph
+V = 5
+# create a 2d array of size 5x5
+# for adjacency matrix to represent graph
+G = [[0, 7, 3, 12, 0],
+     [7, 0, 0, 0, 9],
+     [3, 0, 0, 0, 7],
+     [12, 0, 0, 0, 5],
+     [0, 9, 7, 5, 0]]
+# create a array to track selected vertex
+# selected will become true otherwise false
+selected = [0, 0, 0, 0, 0]
+# set number of edge to 0
+no_edge = 0
+# the number of egde in minimum spanning tree will be
+# always less than(V - 1), where V is number of vertices in
+# graph
+# choose 0th vertex and make it true
+selected[0] = True
+# print for edge and weight
+print("Edge : Cost\n")
+while (no_edge < V - 1):
+    # For every vertex in the set S, find the all adjacent vertices
+    #, calculate the distance from the vertex selected at step 1.
+    # if the vertex is already in the set S, discard it otherwise
+    # choose another vertex nearest to selected vertex  at step 1.
+    minimum = INF
+    x = 0
+    y = 0
+    for i in range(V):
+        if selected[i]:
+            for j in range(V):
+                if ((not selected[j]) and G[i][j]):  
+                    # not in selected and there is an edge
+                    if minimum > G[i][j]:
+                        minimum = G[i][j]
+                        x = i
+                        y = j
+    print(str(x) + "-" + str(y) + ":" + str(G[x][y]))
+    selected[y] = True
+    no_edge += 1
