c8cb874e35c8d58c5c025b6b3436a6fa3ed8b630
mr-shrayan <theemptyunexplored@gmail.com>
2021-06-15 11:37:01 +0530

    Push

--- Laabs/Lab4/Lab5/test.java
+++ Laabs/Lab4/Lab5/test.java
@@ -1,17 +1,20 @@
 class test {
 
     // this method accepts int
-    private static void display(int a){
+    private static int display(int a,int b){
         System.out.println("Got Integer data.");
+        return 0;
     }
 
     // this method  accepts String object
-    private static void display(String a){
+    private static float display(int a,int b){
         System.out.println("Got String object.");
+        float sum = 5.0f+4.2f;
+        return sum;
     }
 
     public static void main(String[] args) {
-        display(1);
-        display("Hello");
+        display(1,2);
+        display(1,2);
     }
 }
