2b7ca45c0380c81e1e9ce61bfac6451b81c27278
mr-shrayan <theemptyunexplored@gmail.com>
2021-06-15 09:39:43 +0530

    test.java

---
+++ Laabs/Lab4/Lab5/test.java
@@ -0,0 +1,17 @@
+class test {
+
+    // this method accepts int
+    private static void display(int a){
+        System.out.println("Got Integer data.");
+    }
+
+    // this method  accepts String object
+    private static void display(String a){
+        System.out.println("Got String object.");
+    }
+
+    public static void main(String[] args) {
+        display(1);
+        display("Hello");
+    }
+}
