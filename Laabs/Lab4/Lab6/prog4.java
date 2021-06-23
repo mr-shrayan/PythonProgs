8d0fffbca345465d99314a2180a470865f3f8494
mr-shrayan <theemptyunexplored@gmail.com>
2021-06-23 12:53:58 +0530

    prog4

---
+++ Laabs/Lab4/Lab6/prog4.java
@@ -0,0 +1,21 @@
+class Animal
+{  
+    String color="white";  
+}  
+    class Dog extends Animal
+    {  
+        String color="black";  
+        void printColor()
+        {  
+            System.out.println(color);//prints color of Dog class  
+            System.out.println(super.color);//prints color of Animal class  
+        }  
+    }  
+    class prog4
+    {  
+        public static void main(String args[])
+        {  
+            Dog d=new Dog();  
+            d.printColor();  
+        }
+}  
