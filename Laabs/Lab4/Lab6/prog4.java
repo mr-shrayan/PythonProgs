e0dee1088287f8d5a03400c82f85e0b75406369b
mr-shrayan <theemptyunexplored@gmail.com>
2021-06-26 22:02:43 +0530

    Programs

--- Laabs/Lab4/Lab6/prog4.java
+++ Laabs/Lab4/Lab6/prog4.java
@@ -1,13 +1,15 @@
+//
 class Animal
 {  
     String color="white";  
 }  
     class Dog extends Animal
     {  
-        String color="black";  
+        String color="black"; 
+
         void printColor()
         {  
-            System.out.println(color);//prints color of Dog class  
+            System.out.println(color);//prints color of Dog class 
             System.out.println(super.color);//prints color of Animal class  
         }  
     }  
